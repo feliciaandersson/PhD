e23eaf92f50c6bcb9355a3bcd8bc456b0812ec5a
Felicia Andersson <felicia.e.andersson@kemi.uu.se>
2024-03-09 13:14:24 +0100

    Added vasp calculator. Added doc strings to the calculators.

--- project/calculation/calculators.py
+++ project/calculation/calculators.py
@@ -6,16 +6,32 @@ import os
 from ase.optimize import LBFGS
 from ase.io.trajectory import Trajectory
 from ase.io import read
+from ase.calculators.vasp import Vasp
 
 
-def DFTB_calculator(atoms, label, calc_type, specification):
+
+def DFTB_calculator(atoms, label, calc_type, parametrization, kpts):
+    """
+    Run DFTB calculation.
+
+    Args:
+        atoms (ase.Atoms): The atomic structure for the calculation.
+        label (str): Label for the calculation.
+        calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
+        specification (str): Parametrization used in the calculation.
+
+    Returns:
+        ase.Atoms: The atomic structure with calculation results.
+    """
 
     calc = Dftb(
         atoms=atoms,
         Driver_="GeometryOptimization",
         Driver_Optimiser="Rational {}",
         Hamiltonian_="xTB",
-        Hamiltonian_Method=f"{specification}-xTB",
+        Driver_LatticeOpt="No",
+        kpts = (1, 1, 1),
+        Hamiltonian_Method=f"{parametrization}-xTB",
         # Hamiltonian_MaxSCCIterations=500,
         # Hamiltonian_SCCTolerance=1e-5,
         label=label,
@@ -27,7 +43,20 @@ def DFTB_calculator(atoms, label, calc_type, specification):
     return atoms
 
 
-def Gaussian_calculator(atoms, label, calc_type, specification):
+def Gaussian_calculator(atoms, label, calc_type, functional, basis_set):
+    """
+    Run Gaussian calculation.
+
+    Args:
+        atoms (ase.Atoms): The atomic structure for the calculation.
+        label (str): Label for the calculation.
+        calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
+        functional (str): functional used in the calculation.
+        basis_set (str): Basis set used in the calculation.
+
+    Returns:
+        ase.Atoms: The atomic structure with calculation results.
+    """
 
     # Check if Gaussian executable is provided as an environment variable
     gaussian_executable = os.environ.get("ASE_GAUSSIAN_COMMAND")
@@ -39,11 +68,11 @@ def Gaussian_calculator(atoms, label, calc_type, specification):
         label=label,
         mem="12GB",
         nprocshared="12",
-        xc="B3LYP",
-        basis=specification,
+        xc=functional,
+        basis=basis_set,
         empiricaldispersion="GD3",
         scf="maxcycle=200",
-        chk=label + ".chk",
+        chk=f"{label}.chk",
         pop="chelpg",
         command=f"{gaussian_executable} < PREFIX.com > PREFIX.log",
     )
@@ -56,5 +85,48 @@ def Gaussian_calculator(atoms, label, calc_type, specification):
 
     elif calc_type.lower() == "sp":
         atoms.get_potential_energy()
+
+    return atoms
+
+def VASP_calculator(atoms, label, calc_type, functional):
+    """
+    Run VASP calculation.
+
+    Args:
+        atoms (ase.Atoms): The atomic structure for the calculation.
+        label (str): Label for the calculation.
+        calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
+        functional (str): Type of functional for the calculation.
+    Returns:
+        ase.Atoms: The atomic structure with calculation results.
+    """
+    
+    nsw = 500 if calc_type.lower() == "opt" else 1
+        
+    calc = Vasp(atoms=atoms,
+                label = label,
+                txt = 'vasp_out',
+                command = 'mpprun vasp_std',
+                algo='normal', 
+                gga = "PE",
+                prec='ACCURATE',
+                istart = 1,
+                icharg = 1,
+                ispin = 1,
+                ivdw = 11,
+                lorbit = None,
+                ediff = 0.1E-06,
+                ediffg = -0.1E-2,
+                nelm = 600,
+                encut = 550.000000,
+                sigma = 0.05,
+                ismear=0,
+                nsw = nsw,
+                ibrion=2,
+                isif=2,
+                )
+    
+    atoms.calc = calc
+    atoms.get_potential_energy()
 
     return atoms
