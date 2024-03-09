3a5896aee3cc082353d8b105badfeba6c4a1c3c3
Felicia Andersson <felicia.e.andersson@kemi.uu.se>
2024-03-09 19:07:48 +0100

    formatted all files.

--- project/analysis/scan.py
+++ project/analysis/scan.py
@@ -3,6 +3,7 @@
 from ase.db import connect
 from ase.io import read, write
 
+
 def separate(atoms):
     indices = range(len(atoms))
     indices_1 = [i for i in indices if atoms.positions[i, 0] < 1]
@@ -14,13 +15,13 @@ def separate(atoms):
 
     return atoms_1, atoms_2
 
+
 def scan(atoms_1, atoms_2, displacement, num_closer, num_away, coordinate):
-    
-    coordinate_indices = {'x': 0, 'y': 1, 'z': 2}
+    coordinate_indices = {"x": 0, "y": 1, "z": 2}
     scanning_points = []
     coordinate_index = coordinate_indices[coordinate]
     print(f"Displacement in the {coordinate} direction.")
-    
+
     start_position = num_closer * displacement
     atoms_1.positions[:, coordinate_index] += start_position
     print(f"Starting position: {start_position} closer than the optimised structure.")
@@ -30,27 +31,31 @@ def scan(atoms_1, atoms_2, displacement, num_closer, num_away, coordinate):
         atoms_combined = atoms_1 + atoms_2
         scanning_points.append(atoms_combined.copy())
         print(f"{iteration}: {atoms_1.positions[0, coordinate_index]}")
-        
+
     return scanning_points
+
 
 def save_to_db(scan_db, scanning_points):
     for iteration, scanning_point in enumerate(scanning_points):
-        scan_db.write(atoms = scanning_point, name = iteration)
+        scan_db.write(atoms=scanning_point, name=iteration)
+
 
 if __name__ == "__main__":
     label = "parallel_x_along_x_6_points"
-    
+
     scan_db = connect(f"./{label}_scan_input.db")
-    
+
     structure = "parallel_x.xyz"
     atoms = read(structure)
-    
+
     atoms_1, atoms_2 = separate(atoms)
-    
+
     coordinate = "x"
     displacement = 0.5
     num_closer = 0
     num_away = 6
-    scanning_points = scan(atoms_1, atoms_2, displacement, num_closer, num_away, coordinate)
-    
+    scanning_points = scan(
+        atoms_1, atoms_2, displacement, num_closer, num_away, coordinate
+    )
+
     save_to_db(scan_db, scanning_points)
--- project/calculation/calculators.py
+++ project/calculation/calculators.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 
+import os
+
 from ase.calculators.dftb import Dftb
 from ase.calculators.gaussian import Gaussian, GaussianOptimizer
-import os
-from ase.optimize import LBFGS
-from ase.io.trajectory import Trajectory
-from ase.io import read
 from ase.calculators.vasp import Vasp
-
+from ase.io import read
+from ase.io.trajectory import Trajectory
+from ase.optimize import LBFGS
 
 
 def DFTB_calculator(atoms, label, calc_type, parametrization, kpts):
@@ -30,7 +30,7 @@ def DFTB_calculator(atoms, label, calc_type, parametrization, kpts):
         Driver_Optimiser="Rational {}",
         Hamiltonian_="xTB",
         Driver_LatticeOpt="No",
-        kpts = (1, 1, 1),
+        kpts=(1, 1, 1),
         Hamiltonian_Method=f"{parametrization}-xTB",
         # Hamiltonian_MaxSCCIterations=500,
         # Hamiltonian_SCCTolerance=1e-5,
@@ -88,6 +88,7 @@ def Gaussian_calculator(atoms, label, calc_type, functional, basis_set):
 
     return atoms
 
+
 def VASP_calculator(atoms, label, calc_type, functional):
     """
     Run VASP calculation.
@@ -100,32 +101,33 @@ def VASP_calculator(atoms, label, calc_type, functional):
     Returns:
         ase.Atoms: The atomic structure with calculation results.
     """
-    
+
     nsw = 500 if calc_type.lower() == "opt" else 1
-        
-    calc = Vasp(atoms=atoms,
-                label = label,
-                txt = 'vasp_out',
-                command = 'mpprun vasp_std',
-                algo='normal', 
-                gga = "PE",
-                prec='ACCURATE',
-                istart = 1,
-                icharg = 1,
-                ispin = 1,
-                ivdw = 11,
-                lorbit = None,
-                ediff = 0.1E-06,
-                ediffg = -0.1E-2,
-                nelm = 600,
-                encut = 550.000000,
-                sigma = 0.05,
-                ismear=0,
-                nsw = nsw,
-                ibrion=2,
-                isif=2,
-                )
-    
+
+    calc = Vasp(
+        atoms=atoms,
+        label=label,
+        txt="vasp_out",
+        command="mpprun vasp_std",
+        algo="normal",
+        gga="PE",
+        prec="ACCURATE",
+        istart=1,
+        icharg=1,
+        ispin=1,
+        ivdw=11,
+        lorbit=None,
+        ediff=0.1e-06,
+        ediffg=-0.1e-2,
+        nelm=600,
+        encut=550.000000,
+        sigma=0.05,
+        ismear=0,
+        nsw=nsw,
+        ibrion=2,
+        isif=2,
+    )
+
     atoms.calc = calc
     atoms.get_potential_energy()
 
