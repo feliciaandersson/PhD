158fb2a04c694c0ef8d67f0986746b8838de02de
Felicia Andersson <felicia.e.andersson@kemi.uu.se>
2024-03-09 13:07:09 +0100

    Added exports and modules for running vasp

--- project/run.sh
+++ project/run.sh
@@ -5,9 +5,9 @@
 #SBATCH --mail-user=felicia.e.andersson@kemi.uu.se
 #SBATCH --cpus-per-task=32
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=dimers
+#SBATCH --job-name=vasp
 #SBATCH --nodes=1
-#SBATCH -t 00:00:30
+#SBATCH --time=00:01:00
 
 # Set neccessary requirements
 ulimit -s unlimited
@@ -15,6 +15,11 @@ export OMP_STACKSIZE=8G
 
 # Load the Gaussian module
 module load Gaussian/16.C.01-avx2-nsc1-bdist
+
+# Set paths and run the VASP module
+export VASP_COMMAND='mpprun vasp_std'
+export VASP_PP_PATH=/proj/teoroo/users/x_felan/VASP/input/
+module add VASP/5.4.4.16052018-nsc2-intel-2018a-eb
 
 # Run the calculation
 python ./calculation/opt.py
