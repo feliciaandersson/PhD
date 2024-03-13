50da7ea9e2a562ea982e12ed34493f34df701169
Felicia Andersson <felicia.e.andersson@kemi.uu.se>
2024-03-13 11:26:09 +0100

    Changed the calculators to work with the new job parameters in the config file. Modified the DFTB calculator to accurately run for both single-point and optimization jobs.

--- src/calculation/calculators.py
+++ src/calculation/calculators.py
@@ -5,38 +5,46 @@ import os
 from ase.calculators.dftb import Dftb
 from ase.calculators.gaussian import Gaussian, GaussianOptimizer
 from ase.calculators.vasp import Vasp
-from ase.io import read
-from ase.io.trajectory import Trajectory
-from ase.optimize import LBFGS
 
 
-def DFTB_calculator(atoms, label, calc_type, parametrization, kpts):
+def DFTB_calculator(atoms, label, calc_type, parametrization, kpts, lattice_opt):
     """
-    Run DFTB calculation.
+    Run a DFTB calculation.
 
     Args:
         atoms (ase.Atoms): The atomic structure for the calculation.
         label (str): Label for the calculation.
         calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
-        specification (str): Parametrization used in the calculation.
+        parametrization (str): Parametrization used in the calculation.
+        kpts (tuple): k-points used in the calculation.
+        lattice_opt (str): Flag indicating whether lattice optimization is enabled.
 
     Returns:
         ase.Atoms: The atomic structure with calculation results.
     """
 
-    calc = Dftb(
-        atoms=atoms,
-        Driver_="GeometryOptimization",
-        Driver_Optimiser="Rational {}",
-        Hamiltonian_="xTB",
-        Driver_LatticeOpt="No",
-        kpts=(1, 1, 1),
-        Hamiltonian_Method=f"{parametrization}-xTB",
+    common_params = {
+        "atoms": atoms,
+        "kpts": (1, 1, 1),
+        "label": label,
+        "Hamiltonian_": "xTB",
+        "Hamiltonian_Method": f"{parametrization}-xTB",
         # Hamiltonian_MaxSCCIterations=500,
         # Hamiltonian_SCCTolerance=1e-5,
-        label=label,
-    )
+    }
 
+    opt_params = {
+        "Driver_": "GeometryOptimization",
+        "Driver_Optimiser": "Rational {}",
+        "Driver_LatticeOpt": "No",
+    }
+
+    if calc_type.lower() == "opt":
+        calc_params = {**common_params, **opt_params}
+    else:
+        calc_params = {**common_params}
+
+    calc = Dftb(**calc_params)
     atoms.calc = calc
     atoms.get_potential_energy()
 
@@ -45,13 +53,13 @@ def DFTB_calculator(atoms, label, calc_type, parametrization, kpts):
 
 def Gaussian_calculator(atoms, label, calc_type, functional, basis_set):
     """
-    Run Gaussian calculation.
+    Run a Gaussian calculation.
 
     Args:
         atoms (ase.Atoms): The atomic structure for the calculation.
         label (str): Label for the calculation.
         calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
-        functional (str): functional used in the calculation.
+        functional (str): Functional used in the calculation.
         basis_set (str): Basis set used in the calculation.
 
     Returns:
@@ -89,20 +97,24 @@ def Gaussian_calculator(atoms, label, calc_type, functional, basis_set):
     return atoms
 
 
-def VASP_calculator(atoms, label, calc_type, functional):
+def VASP_calculator(atoms, label, calc_type, functional, kpts, lattice_opt):
     """
-    Run VASP calculation.
+    Run a VASP calculation.
 
     Args:
         atoms (ase.Atoms): The atomic structure for the calculation.
         label (str): Label for the calculation.
         calc_type (str): Type of calculation, either 'opt' for optimization or 'sp' for single-point.
         functional (str): Type of functional for the calculation.
+        lattice_opt (str): Flag indicating whether lattice optimization is enabled.
+
     Returns:
         ase.Atoms: The atomic structure with calculation results.
     """
 
     nsw = 500 if calc_type.lower() == "opt" else 1
+    # gga = "PE" if functional.upper() == "PBE" else ""
+    isif = 3 if lattice_opt == "yes" else 2
 
     calc = Vasp(
         atoms=atoms,
@@ -110,12 +122,13 @@ def VASP_calculator(atoms, label, calc_type, functional):
         txt="vasp_out",
         command="mpprun vasp_std",
         algo="normal",
-        gga="PE",
+        xc=functional,
         prec="ACCURATE",
         istart=1,
         icharg=1,
         ispin=1,
         ivdw=11,
+        kpts=kpts,
         lorbit=None,
         ediff=0.1e-06,
         ediffg=-0.1e-2,
@@ -125,7 +138,7 @@ def VASP_calculator(atoms, label, calc_type, functional):
         ismear=0,
         nsw=nsw,
         ibrion=2,
-        isif=2,
+        isif=isif,
     )
 
     atoms.calc = calc
